896df45afabdeb3f5ce439ee57aa767de2a7b8c7
Kriti Sen Sharma <ksen@paradigm4.com>
2016-08-08 18:50:10 -0400

    test R script now calculates t statistic

--- test1-eqtl.R
+++ test1-eqtl.R
@@ -1,18 +1,48 @@
+rm(list=ls())
+
+tstat = function(r, n){sqrt(n-2)*r/sqrt(1-r^2)}
+
+calcstat  = function(s, g) {
+  s2 = s-mean(s);               stopifnot(sum(s2) < 1e-10 )
+  s3 = s2 / sqrt(sum(s2^2));    #stopifnot(sum(s3^2) == 1)
+  
+  g2 = g-mean(g);               stopifnot(sum(g2) < 1e-10 )
+  g3 = g2 / sqrt(sum(g2^2));    #stopifnot(sum(g3^2) == 1)
+  
+  # calculate statistic
+  r = s3 %*% g3
+#  print(r)
+  print(tstat(r, length(s)))
+  
+  # Now calculate statistics using R internal functions (example is taken from [base.dir]/demo/a.nocvrt.r)
+  gene.mat=g
+  snps.mat=s
+  lmdl = lm( gene.mat ~ snps.mat );
+  lmout = summary(lmdl)$coefficients[2,c("Estimate","t value","Pr(>|t|)")];
+  print( lmout );
+
+}
+
+
 library(MatrixEQTL)
 
 # Run the program at http://www.bios.unc.edu/research/genomic_software/Matrix_eQTL/R.html#cis to get sample results
-source('/tmp/matrixeqtltest1.R')
-# SNP 5 -- formulated by
-# head -n 6 /Library/Frameworks/R.framework/Versions/3.3/Resources/library/MatrixEQTL/data/SNP.txt | tail -n 1 | tr '\t' '\n' | tail -n +2 | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g'
-snp=c(1,1,2,1,1,2,1,1,0,1,1,2,0,1,2,1)
-# Gene 3 -- formulated by
-# head -n 4 /Library/Frameworks/R.framework/Versions/3.3/Resources/library/MatrixEQTL/data/GE.txt | tail -n 1 | tr '\t' '\n' | tail -n +2 | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/,/g'
-gene=c(12.06,12.29,13.07,13.65,13.86,12.84,12.29,13.03,13.13,14.93,12.40,13.38,13.70,14.49,14.14,13.35)
+base.dir = find.package('MatrixEQTL');
+pathtofile = paste(base.dir, '/demo/sample.all.r', sep = "")
+# source(pathtofile)
+source('script1-gene-snp.R') 
+### The above demo shows that Snp_05 and Gene_03 have highest value for statistic
 
+# Genotype file name
+SNP_file_name = paste(base.dir, "/data/SNP.txt", sep="");
+snptable = read.table(SNP_file_name, row.names = 1, header = TRUE)
 
-snp2 = snp-mean(snp)
-gene2 = gene-mean(gene)
-gene3 = gene2 / sqrt(sum(gene2^2))
-snp3 = snp2 / sqrt(sum(snp2^2))
-r = snp3 %*% gene3
-print(r)
+# Gene expression file name
+expression_file_name = paste(base.dir, "/data/GE.txt", sep="");
+genetable = read.table(expression_file_name, row.names = 1, header = TRUE)
+
+snpnum = 11
+genenum = 6
+s = as.numeric(snptable[sprintf("Snp_%02d", snpnum), ])
+g = as.numeric(genetable[sprintf("Gene_%02d", genenum), ])
+calcstat(s, g)
